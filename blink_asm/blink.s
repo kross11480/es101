5ef96b77fe6c7834bdd84f176c61a340534bc424
kross11480 <hritam79@gmail.com>
2023-04-28 13:49:30 +0200

    + Added Makefile and Readme Assembler code for toggling all the LEDS on button press

--- blink_asm/blink.s
+++ blink_asm/blink.s
@@ -7,22 +7,23 @@
 .space 0xe4
 
 #Initialize Clock for GPIOC, GPIOB, GPIOA
-    ldr r0, =0x00000007
+    ldr r0, =0x00000006
     ldr r1, =0x4002104C
     str r0, [r1]
 
-#Initialize LED0
-#- LEDs: GPIOC->MODER = (GPIOC->MODER & 0xFFFF3FFF) | 0x00004000;
+#Initialize LED 0,1,2,3
+#- LEDs: GPIOC->MODER = (GPIOC->MODER & 0xFFFF00FF) | 0x00005500;
     ldr r1, =0x48000800
     ldr r0, [r1]
-    and r0, #0xFFFF3FFF
-    orr r0, 0x00004000
+    and r0, 0xFFFF00FF
+    orr r0, 0x00005500
     str r0, [r1]
 
-#- switch LED OFF/ON: GPIOC -> ODR |= (1<<7); GPIOC->ODR &= ~( 1 << i );
+#- switch LED: bit value 1 is LED OFF, bit value 0 is LED ON
+#- OFF/ON: GPIOC -> ODR |= (1<<7); GPIOC->ODR &= ~( 1 << i );
     ldr r1, =0x48000814
     ldr r0, [r1]
-    and r0, #0xFFFFFF7F
+    orr r0, 0x00000F0
     str r0, [r1]
 
 b . @ Never ending loop
